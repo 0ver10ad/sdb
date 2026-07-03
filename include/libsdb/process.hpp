23a43244ff817f196fd9e9ebeee87937e2d0eece
Jiayi Li <jiayi.li@aumovio.com>
2026-07-03 11:32:48 +0800

    Initial commit: page 107 SSE complete

---
+++ include/libsdb/process.hpp
@@ -0,0 +1,59 @@
+#ifndef SDB_PROCESS_HPP
+#define SDB_PROCESS_HPP
+
+#include <filesystem>
+#include <memory>
+#include <sys/types.h>
+#include <cstdint>
+#include <libsdb/registers.hpp>
+#include <optional>
+
+namespace sdb {
+	enum class process_state {
+		stopped,
+		running,
+		exited,
+		terminated
+	};
+	struct stop_reason {
+		stop_reason(int wait_status);
+
+		process_state reason;
+		std::uint8_t info;
+	};
+	class process {
+	public:
+		static std::unique_ptr<process> launch(std::filesystem::path path, bool debug = true,
+			std::optional<int> stdout_replacement = std::nullopt);
+		static std::unique_ptr<process> attach(pid_t pid);
+
+		void resume();
+		stop_reason wait_on_signal();
+		pid_t pid() const { return pid_; }
+		process() = delete;
+		process(const process&) = delete;
+		process& operator=(const process&) = delete;
+		~process();
+		process_state state() const { return state_; }
+		registers& get_registers() { return *registers_; }
+		const registers& get_registers() const { return * registers_; }
+		void write_user_area(std::size_t offset, std::uint64_t data);
+		void write_fprs(const user_fpregs_struct& fprs);
+		void write_gprs(const user_regs_struct& gprs);
+	private:
+		pid_t pid_ = 0;
+		bool terminate_on_end_ = true;
+		bool is_attached_ = true;
+		process_state state_ = process_state::stopped;
+		process(pid_t pid, bool terminate_on_end, bool is_attached)
+			: pid_(pid),
+			  terminate_on_end_(terminate_on_end),
+			  is_attached_(is_attached),
+			  registers_(new registers(*this))
+		{}
+		void read_all_registers();
+		std::unique_ptr<registers> registers_;
+	};
+}
+
+#endif
---
+++ test/tests.cpp
@@ -0,0 +1,105 @@
+#include <catch2/catch_test_macros.hpp>
+#include <libsdb/process.hpp>
+#include <sys/types.h>
+#include <signal.h>
+#include <libsdb/error.hpp>
+#include <fstream>
+#include <libsdb/pipe.hpp>
+#include <libsdb/bit.hpp>
+
+using namespace sdb;
+
+namespace {
+	bool process_exists(pid_t pid) {
+		auto ret = kill(pid, 0);
+		return ret != -1 and errno != ESRCH;
+	}
+	char get_process_status(pid_t pid) {
+		std::ifstream stat("/proc/" + std::to_string(pid) + "/stat");
+		std::string data;
+		std::getline(stat, data);
+		auto index_of_last_parenthesis = data.rfind(')');
+		auto index_of_status_indicator = index_of_last_parenthesis + 2;
+		return data[index_of_status_indicator];
+	}
+}
+
+TEST_CASE("process::launch success", "[process]") {
+	auto proc = process::launch("yes");
+	REQUIRE(process_exists(proc->pid()));
+}
+
+TEST_CASE("process::launch no such program", "[process]") {
+	REQUIRE_THROWS_AS(process::launch("you_do_not_have_to_be_good"), error);
+}
+
+TEST_CASE("process::attach success", "[process]") {
+	auto target = process::launch("targets/run_endlessly", false);
+	auto proc = process::attach(target->pid());
+	REQUIRE(get_process_status(target->pid()) == 't');
+}
+
+TEST_CASE("process::attach invalid PID", "[process]") {
+	REQUIRE_THROWS_AS(process::attach(0), error);
+}
+
+TEST_CASE("process::resume success", "[process]") {
+	{
+		auto proc = process::launch("targets/run_endlessly");
+		proc->resume();
+		auto status = get_process_status(proc->pid());
+		auto success = status == 'R' or status == 'S';
+		REQUIRE(success);
+	}
+	{
+		auto target = process::launch("targets/run_endlessly", false);
+		auto proc = process::attach(target->pid());
+		proc->resume();
+		auto status = get_process_status(proc->pid());
+		auto success = status == 'R' or status == 'S';
+		REQUIRE(success);
+	}
+}
+
+TEST_CASE("process::resume already terminated", "[process]") {
+	auto proc = process::launch("targets/end_immediately");
+	proc->resume();
+	proc->wait_on_signal();
+	REQUIRE_THROWS_AS(proc->resume(), error);
+}
+
+TEST_CASE("Write register works", "[register]") {
+	bool close_on_exec = false;
+	sdb::pipe channel(close_on_exec);
+
+	auto proc = process::launch("targets/reg_write", true, channel.get_write());
+	channel.close_write();
+
+	proc->resume();
+	proc->wait_on_signal();
+
+	auto& regs = proc->get_registers();
+	regs.write_by_id(register_id::rsi, 0xcafecafe);
+
+	proc->resume();
+	proc->wait_on_signal();
+
+	auto output = channel.read();
+	REQUIRE(to_string_view(output) == "0xcafecafe");
+
+	regs.write_by_id(register_id::mm0, 0xba5eba11);
+
+	proc->resume();
+	proc->wait_on_signal();
+
+	output = channel.read();
+	REQUIRE(to_string_view(output) == "0xba5eba11");
+
+	regs.write_by_id(register_id::xmm0, 42.24);
+
+	proc->resume();
+	proc->wait_on_signal();
+
+	output = channel.read();
+	REQUIRE(to_string_view(output) == "42.24");
+} 
